9533221012d50d1252c79a40097a3f3462aa2702
AngelFireLA <tinyangel@mail.com>
2025-03-18 13:54:11 +0100

    icone qui grossis

--- interface/menu_sélection_de_jeu.py
+++ interface/menu_sélection_de_jeu.py
@@ -35,6 +35,12 @@ boutton_morpion = boutton.BouttonImagé(positions_icones[2][0], positions_icones
 boutton_mastermot = boutton.BouttonImagé(positions_icones[3][0], positions_icones[3][1], taille_icone, taille_icone, "assets/images/icones_jeux/mastermot.png")
 boutton_mastermind = boutton.BouttonImagé(positions_icones[4][0], positions_icones[4][1], taille_icone, taille_icone, "assets/images/icones_jeux/mastermind.png")
 
+boutton_puissance4_grossi = boutton.BouttonImagé(positions_icones[0][0], positions_icones[0][1], taille_icone + 10, taille_icone + 10, "assets/images/icones_jeux/puissance4.png")
+boutton_échecs_grossi = boutton.BouttonImagé(positions_icones[1][0], positions_icones[1][1], taille_icone + 10, taille_icone + 10, "assets/images/icones_jeux/échecs.png")
+boutton_morpion_grossi = boutton.BouttonImagé(positions_icones[2][0], positions_icones[2][1], taille_icone + 10, taille_icone + 10, "assets/images/icones_jeux/morpion.png")
+boutton_mastermot_grossi = boutton.BouttonImagé(positions_icones[3][0], positions_icones[3][1], taille_icone + 10, taille_icone + 10, "assets/images/icones_jeux/mastermot.png")
+boutton_mastermind_grossi = boutton.BouttonImagé(positions_icones[4][0], positions_icones[4][1], taille_icone + 10, taille_icone + 10, "assets/images/icones_jeux/mastermind.png")
+
 def main():
     clock = pygame.time.Clock()
     fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
@@ -66,16 +72,30 @@ def main():
 
         fenetre.fill((0, 0, 0))
         fenetre.blit(arriere_plan, (0, 0))
-        boutton_puissance4.afficher(fenetre)
+        if boutton_puissance4.rect.collidepoint(pygame.mouse.get_pos()):
+            boutton_puissance4_grossi.afficher(fenetre)
+        else:
+            boutton_puissance4.afficher(fenetre)
         afficher_texte(fenetre, positions_icones[0][0], positions_icones[0][1] + taille_icone // 2 + 20, "Puissance4", 20, dict_couleurs["blanc"])
-
-        boutton_échecs.afficher(fenetre)
+        if boutton_échecs.rect.collidepoint(pygame.mouse.get_pos()):
+            boutton_échecs_grossi.afficher(fenetre)
+        else:
+            boutton_échecs.afficher(fenetre)
         afficher_texte(fenetre, positions_icones[1][0], positions_icones[1][1] + taille_icone//2 + 20, "Echecs", 20, dict_couleurs["blanc"])
-        boutton_mastermot.afficher(fenetre)
+        if boutton_morpion.rect.collidepoint(pygame.mouse.get_pos()):
+            boutton_morpion_grossi.afficher(fenetre)
+        else:
+            boutton_morpion.afficher(fenetre)
         afficher_texte(fenetre, positions_icones[2][0], positions_icones[2][1] + taille_icone//2 + 20, "Morpion", 20, dict_couleurs["blanc"])
+        if boutton_mastermot.rect.collidepoint(pygame.mouse.get_pos()):
+            boutton_mastermot_grossi.afficher(fenetre)
+        else:
+            boutton_mastermot.afficher(fenetre)
         afficher_texte(fenetre, positions_icones[3][0], positions_icones[3][1] + taille_icone//2 + 20, "MasterMot", 20, dict_couleurs["blanc"])
-        boutton_morpion.afficher(fenetre)
-        boutton_mastermind.afficher(fenetre)
+        if boutton_mastermind.rect.collidepoint(pygame.mouse.get_pos()):
+            boutton_mastermind_grossi.afficher(fenetre)
+        else:
+            boutton_mastermind.afficher(fenetre)
         afficher_texte(fenetre, positions_icones[4][0], positions_icones[4][1] + taille_icone//2 + 20, "MasterMind", 20, dict_couleurs["blanc"])
         afficher_texte(fenetre, largeur_fenetre//2, 80, "Mode Arcade", 70, dict_couleurs["bleu marin"])
         pygame.display.update()
